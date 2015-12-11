94c48739addb7c60f8c70cb2157b3a9e1e8ddb6f
Oleg Bogdanov <boguscoder@outlook.com>
2015-12-11 15:10:46 -0800

    Code clean up

--- BinClock/MainActivity.fs
+++ BinClock/MainActivity.fs
@@ -11,29 +11,31 @@ open Android.Runtime
 open Android.Views
 open Android.Widget
 
-type Column = 
-    | HoursFst
-    | HoursSnd
-    | MinutesFst 
-    | MinutesSnd 
-    | SecondsFst 
-    | SecondsSnd
+type ColumnPos = 
+    | HoursFst = 0
+    | HoursSnd = 1
+    | MinutesFst = 2
+    | MinutesSnd = 3
+    | SecondsFst = 4
+    | SecondsSnd = 5
 
+type Column = { Pos:ColumnPos; Size:int }
 
 [<Activity (Label = "Binary Clock", MainLauncher = true, Theme = "@android:style/Theme.NoTitleBar")>]
-type MainActivity () =
+type MainActivity() =
     inherit Activity ()
 
-    let ColumnSpec = [  (HoursFst, 2);
-                        (HoursSnd, 4);
-                        (MinutesFst, 3);
-                        (MinutesSnd, 4);
-                        (SecondsFst, 3);
-                        (SecondsSnd, 4) ]
+    let ColumnSpec = [  {Pos = ColumnPos.HoursFst;   Size = 2};
+                        {Pos = ColumnPos.HoursSnd;   Size = 4};
+                        {Pos = ColumnPos.MinutesFst; Size = 3};
+                        {Pos = ColumnPos.MinutesSnd; Size = 4};
+                        {Pos = ColumnPos.SecondsFst; Size = 3};
+                        {Pos = ColumnPos.SecondsSnd; Size = 4} ]
 
     let mutable gridRoot:ViewGroup = null
     let mutable scaleDetector:ScaleGestureDetector = null
     let mutable scaleFactor = 1.0f
+    let mutable initialItemSize = 0
 
     interface ScaleGestureDetector.IOnScaleGestureListener with
         member this.OnScale detector = 
@@ -48,28 +50,29 @@ type MainActivity () =
 
                 scaleFactor <- min ZOOM_MAX (max ZOOM_MIN (scaleFactor + deltaScale))
 
-                let initialSize = this.Resources.GetDimensionPixelSize(Resource_Dimension.bullet_size)
-                let scaledSize = int(float32(initialSize) * scaleFactor)
+                let scaledSize = int(float32(initialItemSize) * scaleFactor)
      
-                let rescale (item:ImageView) = 
+                let rescale (item:ImageView, idx, column) = 
                     item.LayoutParameters.Height <- scaledSize
                     item.LayoutParameters.Width <- scaledSize
                     item.RequestLayout()
                 this.forEachItem rescale
+
             false
+
         member this.OnScaleBegin detector = true
         member this.OnScaleEnd detector = ()
 
     member this.forEachColumn f = 
-        ColumnSpec |> List.iteri f
+        ColumnSpec |> List.iter f
 
     member this.forEachItem f = 
-        let columnF idx (_, max) = 
-            let columnView = gridRoot.GetChildAt(idx) :?> ViewGroup
-
+        let columnF column = 
+            let columnView = gridRoot.GetChildAt(int column.Pos) :?> ViewGroup
+            let max = column.Size
             for i in 0..max - 1 do
-                let bullet = (columnView.GetChildAt(max - 1 - i)) :?> ImageView
-                bullet |> f
+                let item = (columnView.GetChildAt(max - 1 - i)) :?> ImageView
+                (item, i, column)  |> f
             
         this.forEachColumn columnF
 
@@ -78,7 +81,7 @@ type MainActivity () =
         columnContainer.Orientation <- Orientation.Horizontal
         columnContainer.SetGravity GravityFlags.Bottom
 
-        this.forEachColumn (fun _ (_, size) -> this.createColumn size |> columnContainer.AddView)
+        this.forEachColumn (fun column -> this.createColumn column.Size |> columnContainer.AddView)
 
         let root = this.LayoutInflater.Inflate(Resource_Layout.Main, null) :?> ViewGroup
         root.AddView(columnContainer)
@@ -96,16 +99,8 @@ type MainActivity () =
             
         columnLayout
 
-    // TODO: consider reusing forEachItem
-    member this.setColumnValue idx max value = 
-        let columnView = gridRoot.GetChildAt(idx) :?> ViewGroup
-
-        let testBit bit value = 
-            value &&& (1 <<< bit) <> 0
-
-        for i in 0..max - 1 do
-            let bullet = (columnView.GetChildAt(max - 1 - i)) :?> ImageView
-            bullet.SetImageResource(if (testBit i value) then Resource_Drawable.on else Resource_Drawable.off)
+    member this.setItemValue (bullet:ImageView) value = 
+        bullet.SetImageResource(if value then Resource_Drawable.on else Resource_Drawable.off)
         ()
 
     member this.updateDate = 
@@ -115,18 +110,22 @@ type MainActivity () =
         let second value = 
             value % 10
             
-        let getColumnValueFromDate (column:Column) (nowDate:DateTime) = 
-            match column with
-            | HoursFst -> first nowDate.Hour
-            | HoursSnd -> second nowDate.Hour 
-            | MinutesFst -> first nowDate.Minute
-            | MinutesSnd -> second nowDate.Minute
-            | SecondsFst -> first nowDate.Second
-            | SecondsSnd -> second nowDate.Second            
+        let getColumnValueFromDate column (nowDate:DateTime) = 
+            match column.Pos with
+            | ColumnPos.HoursFst -> first nowDate.Hour
+            | ColumnPos.HoursSnd -> second nowDate.Hour 
+            | ColumnPos.MinutesFst -> first nowDate.Minute
+            | ColumnPos.MinutesSnd -> second nowDate.Minute
+            | ColumnPos.SecondsFst -> first nowDate.Second
+            | ColumnPos.SecondsSnd -> second nowDate.Second   
+            | _ -> 0         
+
+        let testBit bit value = 
+            value &&& (1 <<< bit) <> 0
 
         let now = DateTime.Now
 
-        this.forEachColumn (fun idx (col, size) -> getColumnValueFromDate col now |> this.setColumnValue idx size)
+        this.forEachItem (fun (item, idx, column) -> getColumnValueFromDate column now |> testBit idx |> this.setItemValue item )
         ()
 
     override this.OnTouchEvent(ev) = 
@@ -136,7 +135,7 @@ type MainActivity () =
     override this.OnCreate (bundle) =
         base.OnCreate(bundle)
         let root = this.createGrid
-       
+        initialItemSize <- this.Resources.GetDimensionPixelSize(Resource_Dimension.bullet_size)
         gridRoot <- root.GetChildAt(0) :?> ViewGroup
         scaleDetector <- new ScaleGestureDetector(this, this)
 
